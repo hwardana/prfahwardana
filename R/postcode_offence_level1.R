7e4728a73c8cbe5bb972f7dae92f3248585ee9b8
User <harrywardana@gmail.com>
2017-11-09 11:52:57 +1030

    t

--- R/postcode_offence_level1.R
+++ R/postcode_offence_level1.R
@@ -1,17 +1,21 @@
-## QUESTION ----
-
 #' postcode_offencelvl1
 #'
-#' \code{<postcode_offencelvl1>} <... does what? A short description.>
+#' \code{<postcode_offencelvl1>} a function that returns a graph of monthly total offences
 #' @param crime_data A data.table object with the following columns:
 #'     "date" (POSIXct), "suburb" (chr), "postcode" (chr), "offence_level_1" (chr),
 #'     "offence_level_2" (chr), "offence_level_3" (chr), "offence_count" (num).
-#' @param offence_description A character string of <What are your expected inputs?>.
+#' @param offence_description A character string of "offence_level_1"
 #' @param postcodes A two-element character vector. Each element is an SA postcode.
 #' @export
-#' @return  A ggplot object showing the correlation in offence count between the two input postcodes.
+#' @return  A ggplot object showing the monthly total offences of the input offence_level_1 in the two input postcodes.
 #' @examples
-#' <one or two examples showing how to use the function>
+#' offence_description <- "ACTS INTENDED TO CAUSE INJURY"
+#' postcodes <- c("5006", "5082")
+#' crime_data_12 <- setDT(read_excel("data/crime-statistics-2012-13.xlsx"))
+#' setnames(crime_data_12, c("date", "suburb", "postcode", "offence_level_1",
+#'                          "offence_level_2", "offence_level_3", "offence_count"))
+#'
+#' postcode_offencelvl1(crime_data, offence_description, postcodes)
 #'
 #'
 
